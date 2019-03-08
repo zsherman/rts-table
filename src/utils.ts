649e717ae1229290e3b941398e679bf6ea6a4c08
Zach Sherman <zksherm@gmail.com>
2019-03-07 20:57:27 -0500

    update module location

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,8 @@
-export function getPageRange(currentPage: number, pageSize: number, count: number) {
+export function getPageRange(
+  currentPage: number,
+  pageSize: number,
+  count: number,
+) {
   const currentPageIdx = currentPage - 1;
   const startIndex = currentPageIdx * pageSize;
   const stopIndex = startIndex + (pageSize - 1);
